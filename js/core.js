24bae14a3fe08db95b6a131a2f7159e725e13a36
Andrey Petrov <andrey.petrov@shazow.net>
2009-04-03 02:32:55 -0400

    Added permalink support

--- js/core.js
+++ js/core.js
@@ -2,8 +2,7 @@
 
 API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&callback=?"
 
-function do_query() {
-    var name = $("#name").val();
+function do_query(name) {
     var query = "\"" + name + " was caught\"";
     var target_url = API_URL + "&q=" + query;
     $.getJSON(target_url, function(data) { render_results(data, name) });
@@ -33,3 +32,29 @@ function render_results(data, name) {
         $("#results ul:first").append('<li class="style' + ((i % 6) + 1 )+ '">' +  convert_content(result.content, name) + '</li>');
     });
 }
+
+/* Permalinks: */
+
+function go_permalink() {
+    var name = $("#name").val();
+    window.location.hash = '#' + name;
+    check_hash();
+}
+
+function decode_hash(input) {
+    return input.substring(1);
+}
+
+function handle_hash() {
+    var name = decode_hash(window.location.hash);
+    $("#name").val(name);
+    do_query(name);
+}
+
+var current_hash = "";
+function check_hash() {
+    if (current_hash == window.location.hash) return;
+    current_hash = window.location.hash;
+    handle_hash();
+}
+setInterval("check_hash()", 200);
