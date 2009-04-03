657d282cf9152f583fdee6215adddcf3aa3ba221
Andrey Petrov <andrey.petrov@shazow.net>
2009-04-03 01:51:10 -0400

    Working-ish

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 /* core.js - iwascaught.com core javascript functions */
 
-API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&callback=?"
+API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&callback=?"
 
 function do_query() {
     var name = $("#name").val();
@@ -9,12 +9,16 @@ function do_query() {
     $.getJSON(target_url, function(data) { render_results(data, name) });
 }
 
+var RE_FIX_SPACES = new RegExp('\\s+', 'g');
 var RE_STRIP_HTML = new RegExp('<.+?>', 'g');
 var RE_PERIOD = new RegExp('\\.');
 
+
 function convert_content(content, name) {
-    var re_name = new RegExp(name + ' was caught', 'i');
-    var s = content.replace(RE_STRIP_HTML, '');
+    var re_name = new RegExp(name + '\\W+was\\W+caught');
+    var s = content.toLowerCase();
+    s = s.replace(RE_STRIP_HTML, '');
+    s = s.replace(RE_FIX_SPACES, ' ');
     s = s.substring(s.search(re_name));
     s = s.replace(re_name, '<strong>' + name + ' was caught</strong>');
     s = s.substring(0, s.search(RE_PERIOD));
@@ -26,7 +30,6 @@ function render_results(data, name) {
 
     $("#results").html("<ul></ul>");
     $.each(data.responseData.results, function(i, result) {
-        console.log(name);
         $("#results ul:first").append('<li class="style' + ((i % 6) + 1 )+ '">' +  convert_content(result.content, name) + '</li>');
     });
 }
